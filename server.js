cc07a856c2256bd417a5aead837286fd553c266f
sailendrakondapalli <127012315+sailendrakondapalli@users.noreply.github.com>
2025-07-26 09:23:18 +0530

    Update server.js

--- server.js
+++ server.js
@@ -10,11 +10,6 @@ require("dotenv").config();
 const app = express();
 app.use(cors());
 app.use(express.json());
-const path = require("path");
-app.use(express.static(path.join(__dirname, "build")));
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "build", "index.html"));
-});
 
 // ===== MongoDB Connection =====
 mongoose.connect(
